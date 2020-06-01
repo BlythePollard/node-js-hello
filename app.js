3324c7fcb577cf0e65f751f67fbc2d3262eb5a49
BlythePollard <github email address>
2020-05-31 19:16:54 -0600

    added custom module

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const http = require('http');
-
+var http = require('http');
+var dt = require('./myfirstmodule');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+const server = http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("The date and time are currently: " + dt.myDateTime());
+  res.end();
+})
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
---
+++ notes.md
@@ -0,0 +1,6 @@
+Modules: like JS libraries-- a set of functions to include in application
+    -To include a module: use require, ie http = require('http) gives access to
+     http module and can create server
+    -Can create own modules using exports.moduleName = function () {return x}
+        -save this in own file, exports keyword makes it available outside module file
+    
